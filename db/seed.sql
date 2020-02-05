7225fe349eb7b2e5dbdcc057758e38ea67e8eda0
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2020-02-05 13:32:43 +0000

    seeded database

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers  (name) VALUES ('Vegan double "cheese" burger');
+INSERT INTO burgers  (name) VALUES ('Chicken burger');
+INSERT INTO burgers  (name) VALUES ('Veggie bean burger');
+INSERT INTO burgers  (name) VALUES ('Bacon cheese burger');
